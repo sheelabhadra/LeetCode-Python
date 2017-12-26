92db80c9b8467a4905e1f1f1c0979020570afd0b
Sheelabhadra Dey <sheelabhadra@gmail.com>
2017-12-26 15:32:55 +0530

    Skip the node to be deleted

---
+++ 203_Remove_Linked_List_Elements.py
@@ -0,0 +1,33 @@
+#Remove all elements from a linked list of integers that have value val.
+
+#Example
+#Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+#Return: 1 --> 2 --> 3 --> 4 --> 5
+
+#### SOLUTION ####
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head and head.val == val:
+            head = head.next
+        
+        dummy = head
+        
+        while dummy and dummy.next:
+            if dummy.next.val == val:
+                dummy.next = dummy.next.next
+            else:
+                dummy = dummy.next
+        return head
+        
