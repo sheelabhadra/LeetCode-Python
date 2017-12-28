7981641f6de08e6bb287943083373fbcf4b636ff
Sheelabhadra Dey <sheelabhadra@gmail.com>
2017-12-28 16:52:05 +0530

    Iterative using stack

---
+++ 94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,35 @@
+#Given a binary tree, return the inorder traversal of its nodes' values.
+
+#For example:
+#Given binary tree [1,null,2,3],
+#   1
+#    \
+#     2
+#    /
+#   3
+
+#### SOLUTION ####
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
