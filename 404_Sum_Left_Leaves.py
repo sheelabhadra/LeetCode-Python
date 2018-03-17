0e63de6fe868eadf8d2c59992adaa16e59748fff
Sheelabhadra Dey <sheelabhadra@gmail.com>
2018-03-16 22:13:49 -0500

    feat: Check if root.left is not None

---
+++ 404_Sum_Left_Leaves.py
@@ -0,0 +1,35 @@
+#Find the sum of all left leaves in a given binary tree.
+
+#Example:
+
+#    3
+#   / \
+#  9  20
+#    /  \
+#   15   7
+
+#There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+## SOLUTION: Check if root.left is non empty. If non-empty and both its children are None 
+# then return its value and add it with recursive call on root.right
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        
+        if root.left != None and root.left.left == None and root.left.right == None:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
