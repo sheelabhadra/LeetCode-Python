a0d40dc8a2da427059df1047e39b46d2ab607623
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-03-04 19:09:38 -0600

    Contest-126: Using sliding window

---
+++ 1004_Max_Consecutive_Ones_III.py
@@ -0,0 +1,21 @@
+"""Problem:
+Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+
+Return the length of the longest (contiguous) subarray that contains only 1s. 
+"""
+
+"""Solution: #copied
+For each A[j], try to find the longest subarray.
+If A[i] ~ A[j] has zeros <= K, we continue to increment j.
+If A[i] ~ A[j] has zeros > K, we increment i.
+"""
+
+class Solution:
+    def longestOnes(self, A: List[int], K: int) -> int:
+        i = 0
+        for j in range(len(A)):
+            K -= 1 - A[j]
+            if K < 0:
+                K += 1 - A[i]
+                i += 1
+        return j-i+1
