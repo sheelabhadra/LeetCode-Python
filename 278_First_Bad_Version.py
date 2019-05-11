8fe17985aff17171e1119028e0bf67825698bcb8
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-10 20:56:23 -0500

    Using binary search

---
+++ 278_First_Bad_Version.py
@@ -0,0 +1,41 @@
+"""PROBLEM:
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version 
+of your product fails the quality check. Since each version is developed based on the previous version, all the versions
+after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones
+to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the
+first bad version. You should minimize the number of calls to the API.
+
+"""
+
+"""SOLUTION:
+Use binary search.
+
+"""
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lo = 0
+        hi = n
+
+        while lo <= hi:
+            mid = lo + (hi-lo)// 2
+            if not isBadVersion(mid) and isBadVersion(mid+1):
+                return mid+1
+            if isBadVersion(mid):
+                hi = mid
+            elif not isBadVersion(mid):
+                lo = mid + 1
+                
