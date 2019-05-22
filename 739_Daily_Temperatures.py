0c66af80a3b6af809129446493a3e69f1ee321e7
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-21 21:11:05 -0500

    Revisited the problem and had loads of frustration

--- 739_Daily_Temperatures.py
+++ 739_Daily_Temperatures.py
@@ -1,12 +1,52 @@
-# Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days
-# you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+"""PROBLEM:
+Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days
+you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
 
-# For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], 
-# your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], 
+your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+"""
 
-## SOLUTION: Start from the end of the temperatures list.
-# And use a stack to keep track of the next biggest element
+"""SOLUTION:
+Iterate from the end of the list. Use a stack. If the current element is less than the top of the stack, add the 
+element to the stack along with the number of days (=1). When an element is encountered that has a value greater
+than the top of the stack, pop from the stack until a value greater than the element is obtained. Keep adding the
+number of days on every pop. See the actual implementation for other minute details.
 
+"""
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        T = T[::-1]
+        stack = [(T[0],0)]
+        res = [0]
+        i = 1
+    
+        while T[i] >= T[i-1]:
+            stack.append((T[i], 0))
+            res.append(0)
+            i += 1 
+        
+        while i < len(T):
+            if T[i] < stack[-1][0]:
+                stack.append((T[i], 1))
+                res.append(1)
+                i += 1
+            else:
+                days = 1
+                while stack and T[i] >= stack[-1][0]:
+                    days += stack[-1][1]    
+                    stack.pop()     
+                if not stack:
+                    stack.append((T[i], 0))
+                    res.append(0)
+                else:
+                    stack.append((T[i], days))
+                    res.append(days)
+                i += 1
+        
+        return res[::-1]
+                
+# More elegant solution - copied
 class Solution(object):
     def dailyTemperatures(self, T):
         """
