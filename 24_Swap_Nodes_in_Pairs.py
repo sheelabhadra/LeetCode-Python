3a997e821da7a4d57043a05f90a9ec948befa2bc
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-27 21:01:07 -0500

    Recursive solution

---
+++ 24_Swap_Nodes_in_Pairs.py
@@ -0,0 +1,19 @@
+"""PROBLEM:
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+"""
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
