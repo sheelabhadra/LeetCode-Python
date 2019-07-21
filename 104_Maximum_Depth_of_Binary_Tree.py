c52f06cf2cb4f0a368e1e6c427d38677de5a055e
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-07-20 19:38:29 -0500

    Tail recursive solution

--- 104_Maximum_Depth_of_Binary_Tree.py
+++ 104_Maximum_Depth_of_Binary_Tree.py
@@ -6,11 +6,23 @@ The maximum depth is the number of nodes along the longest path from the root no
 Note: A leaf is a node with no children.
 
 """
+# Tail Recursive solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-        
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return max(self.helper(root.left, 1), self.helper(root.right, 1))
+    
+    def helper(self, node, depth):
+        if not node:
+            return depth
+        depth += 1
+        return max(self.helper(node.left, depth), self.helper(node.right, depth))
         
