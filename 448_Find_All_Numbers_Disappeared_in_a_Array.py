e5793f682e3d0aeb7261b7f778cb673fb7c2e8b4
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-07-26 18:26:12 -0500

    Hashset of all the numbers

---
+++ 448_Find_All_Numbers_Disappeared_in_a_Array.py
@@ -0,0 +1,23 @@
+"""SOLUTION:
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+"""
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        count = [0]*len(nums)
+        for n in nums:
+            count[n-1] += 1
+        
+        res = []
+        print(count)
+        for i,n in enumerate(count):
+            if n == 0:
+                res.append(i+1)
+        
+        return res
+                
