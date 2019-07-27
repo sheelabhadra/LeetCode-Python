42c2644e130c4ae4d9631182c29ea64e76f4a3da
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-07-26 20:33:26 -0500

    Skip target value

---
+++ 27_Remove_Element.py
@@ -0,0 +1,30 @@
+"""PROBLEM:
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+Given nums = [3,2,2,3], val = 3,
+Your function should return length = 2, with the first two elements of nums being 2.
+It doesn't matter what you leave beyond the returned length.
+
+"""
+
+"""SOLUTION:
+The idea is to keep only those elements which are not equal to the target. So we keep a counter `i` that is only
+incremented when the value in the list is not equal to the target value. When we encounter the target value we just
+skip it.
+
+"""
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+                
