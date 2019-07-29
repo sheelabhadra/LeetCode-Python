0232b8883e5f24c94bd6a6a27e4c9b53e56aabc1
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-07-29 12:47:10 -0500

    Added DP solution

--- 118_Pascal's_Triangle.py
+++ 118_Pascal's_Triangle.py
@@ -8,6 +8,7 @@ Using recursion.
 
 """
 
+"""
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 0:
@@ -27,4 +28,18 @@ class Solution:
         pas = [[1]]
         addrow(numRows, [1])
         return pas
+ """
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1,1]]
         
+        pascal_tr = [[1]*x for x in range(1, numRows+1)]
+        for row in range(2, numRows):
+            for col in range(1, row):
+                pascal_tr[row][col] = pascal_tr[row-1][col-1] + pascal_tr[row-1][col]
+        return pascal_tr
+                
