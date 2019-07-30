3df356c14d6ce5630a37703d270f80e6bf601943
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-07-29 19:45:38 -0500

    Skip when adjacent elements are equal

---
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,21 @@
+"""PROBLEM:
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+"""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        i = 0
+        for j in range(len(nums)-1):
+            if nums[j] != nums[j+1]:
+                nums[i] = nums[j]
+                i += 1
+        
+        nums[i] = nums[-1]
+        return i + 1
+        
