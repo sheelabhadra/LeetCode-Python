4533f67f5d6fcd7db7484082a42e6504251fa3b4
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-08-01 23:50:35 -0500

    Added alternate solution

--- 66_Plus_One.py
+++ 66_Plus_One.py
@@ -25,3 +25,35 @@ class Solution(object):
             digits[0] = 1
             digits.append(0)
         return digits
+
+# Alternate solution
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if not digits:
+            return [1]
+        
+        carry = 0
+        if digits[-1] == 9:
+            digits[-1] = 0
+            carry = 1
+        else:
+            digits[-1] += 1
+
+        for i in range(len(digits)-2, -1, -1):
+            if carry:
+                if digits[i] == 9:
+                    digits[i] = 0
+                    carry = 1
+                else:
+                    digits[i] += 1
+                    carry = 0
+        
+        if carry == 1:
+            digits.insert(0, 1)
+
+        return digits
+        
